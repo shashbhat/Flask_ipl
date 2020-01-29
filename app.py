cd01542d7449f488990206b430382c84d51525fc
Shashank K Bhat <shashank.kakunje@gmail.com>
2020-01-29 10:55:39 +0530

    2

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask , render_template
+# Importing flask module in the project is mandatory 
+# An object of Flask class is our WSGI application. 
+from flask import Flask 
 
-app = Flask(__name__)
+# Flask constructor takes the name of 
+# current module (__name__) as argument. 
+app = Flask(__name__) 
 
-@app.route("/")
-def home():
-    print('Hello world')
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function. 
+@app.route('/') 
+# ‘/’ URL is bound with hello_world() function. 
+def hello_world(): 
+	return 'Hello World'
 
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+# main driver function 
+if __name__ == '__main__': 
+
+	# run() method of Flask class runs the application 
+	# on the local development server. 
+	app.run() 
